f50a8fc7e8538aa15f2e08852c5153dd19d160b3
Seyifunmi Owoeye <seyifunmiowoeye@gmail.com>
2025-02-27 13:39:52 -0500

    Updated function to accept the file path to geneExprMat as an input. It checks if the file exists, and if not it generates the matrix

--- julia_fxns/CombineTRN/combineTRN2.jl
+++ julia_fxns/CombineTRN/combineTRN2.jl
@@ -5,44 +5,53 @@ using DelimitedFiles
 using JLD2
 using NamedArrays
 
-function combineGRNs2(dirOutput, normGeneExprFile, targGeneFile, potRegFile, tfaGeneFile, priorFile, edgeSS, minTargets)
-try
-    mkdir(dirOutput)
-catch
-    ##
-end
-include("/data/miraldiNB/Katko/Projects/Julia/Inferelator_Julia/julia_fxns/importGeneExpGeneLists.jl")
-include("/data/miraldiNB/Katko/Projects/Julia/Inferelator_Julia/julia_fxns/integratePrior_estTFA.jl")
-include("/data/miraldiNB/Katko/Projects/Julia/Inferelator_Julia/julia_fxns/estimateInstabilitiesTRNbStARS.jl")
-include("/data/miraldiNB/Katko/Projects/Julia/Inferelator_Julia/julia_fxns/buildTRNs_mLassoStARS.jl")
+include("../importGeneExpGeneLists2.jl")
+include("../integratePrior_estTFA.jl")
+include("../estimateInstabilitiesTRNbStARS.jl")
+include("../buildTRNs_mLassoStARS.jl")
 
+function combineGRNs2(dirOutput, normGeneExprFile, targGeneFile, potRegFile, 
+                        tfaGeneFile, priorFile, edgeSS, minTargets; geneExprMat = nothing)
+    # function combineGRNs2(dirOutput, normGeneExprFile, targGeneFile, potRegFile, tfaGeneFile, priorFile, edgeSS, minTargets)
+    try
+        mkdir(dirOutput)
+    catch
+        ##
+    end
 
-# 1. Import gene expression data, list of regulators, list of target genes into a Matlab .jld object
-tfaMat= dirOutput * "/tfaMat.jld"
-geneExprMat = dirOutput * "/geneExprMat.jld"
+    # 1. Import gene expression data, list of regulators, list of target genes into a Matlab .jld object
+    tfaMat= dirOutput * "/tfaMat.jld"
 
-# 1. Import gene expression data
-println("1. importGeneExpGeneLists.m")
-importGeneExpGeneLists(normGeneExprFile,targGeneFile,potRegFile,geneExprMat,tfaGeneFile)
+    # Set geneExprMat path
+    if geneExprMat === nothing
+        geneExprMat = dirOutput * "/geneExprMat.jld"
+    end
 
-# 2. Integrate prior information for TFA estimation
-println("2. integratePrior_estTFA.m")
-integratePrior_estTFA(geneExprMat,priorFile,minTargets,edgeSS,tfaMat)
+    # 1. Import gene expression data
+    println("1. importGeneExpGeneLists.m")
+    if !isfile(geneExprMat)
+        println("Loading gene expression data once...")
+        importGeneExpGeneLists(normGeneExprFile, targGeneFile, potRegFile, geneExprMat, tfaGeneFile)
+    end
 
-# Save TFA as a text file
-medTfas = load(tfaMat, "medTfas")
-conditionsc = load(geneExprMat, "conditionsc")
-#conditionsc = conditionsc[1:length(conditionsc)-1]
-pRegs = load(tfaMat, "pRegs")
+    # 2. Integrate prior information for TFA estimation
+    println("2. integratePrior_estTFA.m")
+    integratePrior_estTFA(geneExprMat,priorFile,minTargets,edgeSS,tfaMat)
 
-medTfas_named = NamedArray(medTfas)
-setnames!(medTfas_named, pRegs, 1)
-setnames!(medTfas_named, conditionsc, 2)
+    # Save TFA as a text file
+    medTfas = load(tfaMat, "medTfas")
+    conditionsc = load(geneExprMat, "conditionsc")
+    #conditionsc = conditionsc[1:length(conditionsc)-1]
+    pRegs = load(tfaMat, "pRegs")
 
-outputfile = dirOutput * "/TFA.txt"
-open(outputfile, "w") do io
-    writedlm(io, permutedims(conditionsc))
-    writedlm(io, medTfas_named)
-end
+    medTfas_named = NamedArray(medTfas)
+    setnames!(medTfas_named, pRegs, 1)
+    setnames!(medTfas_named, conditionsc, 2)
+
+    outputfile = dirOutput * "/TFA.txt"
+    open(outputfile, "w") do io
+        writedlm(io, permutedims(conditionsc))
+        writedlm(io, medTfas_named)
+    end
 
 end
