1a8d10d68b441ea46c4edfecd90af994fc721696
Seyifunmi Owoeye <seyifunmiowoeye@gmail.com>
2025-04-10 15:23:13 -0400

    Improved script workflow and error handling

--- Testing/workflow.bat
+++ Testing/workflow.bat
@@ -1,10 +1,13 @@
 #BSUB -n 25
-#BSUB -W 5:00
-#BSUB -M 200000
-#BSUB -e "/data/miraldiNB/Katko/Projects/Julia/Inferelator_Julia/outputsMichael/log.err"
+#BSUB -W 100:00
+#BSUB -M 250000
+#BSUB -e "/data/miraldiNB/Michael/Scripts/Inferelator_JL/outputsMichael/log.err"
+#BSUB -o "/data/miraldiNB/Michael/Scripts/Inferelator_JL/outputsMichael/logOutput.out"
 
-module load julia/1.6.2-wrl
+module purge
+module load julia/1.7.3
 
-cd /data/miraldiNB/Katko/Projects/Julia/Inferelator_Julia/Testing
+cd /data/miraldiNB/Michael/Scripts/Inferelator_JL/Testing
 
 julia --threads 25 workflowCombined.jl
+
--- Testing/workflowCombined.jl
+++ Testing/workflowCombined.jl
@@ -1,4 +1,4 @@
-#using Base: Float16
+# using Base: Float16
 ## example_workflow_Th17
 # Use mLASSO-StARS to build a TRN from gene expression and prior
 # information in four steps. Please refer to each function's help
@@ -27,109 +27,140 @@ include("../julia_fxns/CombineTRN/combineTRN1.jl")
 include("../julia_fxns/CombineTRN/combineTRN2.jl")
 
 tfaOptions = ["", "TFmRNA"]
-networkBaseName = "ZiTest_022525_2" # Name of network
+#output dir
+outputDir = "../outputsMichael/ATACprior/SC/"
+
 ## Inputs
+
 # Normalized gene expression matrix (genes x Pseudobulk). Often normalized with DESeq2
-#normGeneExprFile = "/data/miraldiNB/Katko/Projects/Barski_CD4_Multiome/Outs/Pseudobulk/RNA2/counts_combatseq_vst.txt"
-normGeneExprFile = "/data/miraldiNB/Katko/Projects/Julia/ZiData/IBD_multiome_CD4T_DEseq2_CellTypeDiseaseTissueStatus_vst_counts_5pct_min.txt"
+# normGeneExprFile = "/data/miraldiNB/wayman/projects/Tfh10/outs/202404/pseudobulk/pseudobulk_scrna/CellType/Age/Factor1/min0.25M/counts_Tfh10_AgeCellType_pseudobulk_scrna_vst_batch_NoState.txt"
+normGeneExprFile = "/data/miraldiNB/Michael/GRN_Benchmark/Data/geneExpression/Tfh10_scRNA_10KCells_logNorm_Counts.arrow"
 
 # List of target genes
-#targGeneFile = "/data/miraldiNB/Katko/Projects/Barski_CD4_Multiome/Outs/Seurat/Pseudobulk_RNA/SigGenes2/celltype_log2FC0p58_FDR10/sig_genes.txt"
-targGeneFile = "/data/miraldiNB/Katko/Projects/Julia/ZiData/IBD_enhancer_target_DE_gene_combined_5pct_min.txt"
-
+targGeneFile = "/data/miraldiNB/wayman/projects/Tfh10/outs/202404/GRN_NoState/inputs/target_genes/gene_targ_Tfh10_SigPct5Log2FC0p58FDR5.txt"
+            
 # List of TFs
-#potRegFile = "/data/miraldiNB/Katko/Projects/Barski_CD4_Multiome/Outs/GRN/pot_regs_test.txt"
-potRegFile = "/data/miraldiNB/Katko/Projects/Julia/ZiData/TF_CD4T_sig_celltype_status_minFrac5_RELI_targets_TFA_ANOVA_Pval_FDR5.txt"
-
+potRegFile = "/data/miraldiNB/wayman/projects/Tfh10/outs/202404/GRN_NoState/inputs/pot_regs/TF_Tfh10_SigPct5Log2FC0p58FDR5_final.txt"
+           
 # List of genes to calculate TFA for. Leave empty for all genes (typical)
 tfaGeneFile = ""
 
 # Prior matrix
-#priorFile = "/data/miraldiNB/Katko/Projects/Barski_CD4_Multiome/Outs/Prior_MaxATAC/032324/MaxATAC_Combined_b.tsv"
-priorFile = "/data/miraldiNB/Katko/Projects/Julia/ZiData/IBD_CD4T_ATAC_motif_scan_maxATAC_prior.tsv"
+priorFile = "/data/miraldiNB/Michael/Scripts/Inferelator_JL/Tfh10_Example/inputs/priors/ATAC/ATAC_Tfh10.tsv"  #ATAC
 
+# Degenerate TF-merged file
+priorMergedTfsFile = "/data/miraldiNB/Michael/Scripts/Inferelator_JL/Tfh10_Example/inputs/priors/ATAC/_mergedTfs.txt" # Empty if you dont have a merged TF file
 
 # List of prior files for penalties
-priorFile_penalties = ["/data/miraldiNB/Katko/Projects/Julia/ZiData/IBD_CD4T_ATAC_motif_scan_maxATAC_prior.tsv", 
-                        "/data/miraldiNB/Katko/Projects/Julia/ZiData/Dorothea_binary_prior.tsv"]
+priorFilePenalties = ["/data/miraldiNB/Michael/Scripts/Inferelator_JL/Tfh10_Example/inputs/priors/ATAC/ATAC_Tfh10.tsv" ]
 
-
+baseName = "10KCells" # Name of directory to save results [if "", pipeline automatically creates basename using the parameters below]
 ## Parameters 
-totSS = 50 # Build this many subsampled networks. Typically 50-100. High number may lead to more stable predictions but longer runtime
+totSS = 220 # Build this many subsampled networks. Typically 50-100. High number may lead to more stable predictions but longer runtime
 edgeSS = 0  # Subsample edges for TFA. 0 for no subsampling (typically 0)
 minTargets = 3 # Min targets a TF should have in prior (typically 3)
-lambdaBias = [.75,.25] # List of penalties applied to each non-prior supported interactions (typically 0.5)
+lambdaBias = [0.50] # Penalty applied to non-prior supported interactions (typically 0.5)
 targetInstability = .05 # bStARs instability parameter (typically 0.05)
 lambdaMin = .01 # Min lambda to consider (typically 0.01)
 lambdaMax = 1 # Max lambda to consider (typically 1)
 extensionLimit = 1
 totLogLambdaSteps = 10 # will have this many steps per log10 within bStARS lambda range
 bStarsTotSS = 3 # Number of subsamples used to estimate lambda range. (typically 3-10)
-subsampleFrac = 0.63 # Number of pseudobulks (or cells) to use in each subsample. (1/e = 0.63 is typical but may depend on dataset)
+subsampleFrac = 0.13 # Number of pseudobulks (or cells) to use in each subsample. (1/e = 0.63 is typical for pseudobulk data)
 leaveOutSampleList = "" # Empty for no leaveout
 leaveOutInf = "" # Empty for no leaveout
-correlation_weight = 1 # How heavy to weight correlation (typically 1)
-priorMergedTfsFile = "" # Empty if you dont have a merged TF file
-meanEdgesPerGene = 10 # Average number of TFs that regulate each gene. Effects size of final network (typically 10) 
+correlationWeight = 1 # How heavy to weight correlation (typically 1)
+meanEdgesPerGene = 20 # Average number of TFs that regulate each gene. Effects size of final network (typically 10) . Using 20 for the sake of benchmarking
 combineOpt = "max" # Either max or mean (typically max)
+
+
+subsamplePct = subsampleFrac * 100
+subsampleStr = isinteger(subsamplePct) ? string(Int(subsamplePct)) : replace(string(subsamplePct), "." => "p")
+# lambdaStr = replace(string(lambdaBias), "." => "p")
+lambdaStr = join(replace.(string.(lambdaBias), "." => "p"), "_")
+networkBaseName = "lambda" * lambdaStr * "_" * string(totSS) * "totSS_" *
+                  string(meanEdgesPerGene) * "tfsPerGene_"  * "subsamplePCT" * subsampleStr 
+if baseName == ""
+    dirOut = joinpath(outputDir,networkBaseName)
+    mkpath(dirOut)
+else
+    dirOut = joinpath(outputDir, baseName, networkBaseName )
+    mkpath(dirOut) 
+end
+
+## 1. Import gene expression data, list of regulators, list of target genes
+println("1. importGeneExpGeneLists.jl")
+geneExprMat = joinpath(dirOut,"geneExprMat.jld")
+if !isfile(geneExprMat)
+    println("Loading gene expression data once...")
+    importGeneExpGeneLists(normGeneExprFile, targGeneFile, potRegFile, geneExprMat, tfaGeneFile)
+end
+
+## 2. Given a prior of TF-gene interactions, estimate transcription factor activities (TFAs) using prior-based TFA and TF mRNA levels
+println("2. integratePrior_estTFA.jl")
+tfaMat = joinpath(dirOut, "tfaMat.jld")
+if !isfile(tfaMat)
+    println("Loading TFA matrix once...")
+    integratePrior_estTFA(geneExprMat,priorFile,minTargets,edgeSS, tfaMat)
+end
 
 for i in 1:2
     if i == 1
-        Network_Name = networkBaseName * "_TFA"
+        instabilitiesDir =  joinpath(dirOut ,"TFA")
     else
-        Network_Name = networkBaseName * "_TFmRNA"
+        instabilitiesDir = joinpath(dirOut ,"TFmRNA")
     end
     tfaOpt = tfaOptions[i]
-    ## 1. Import gene expression data, list of regulators, list of target genes
-    instabilitiesDir = "../outputsMichael/" * Network_Name 
+    mkdir(instabilitiesDir)
+
     try
         mkdir(instabilitiesDir)
     catch
         ##
     end
-    println("1. importGeneExpGeneLists.jl")
-    geneExprMat = instabilitiesDir * "/geneExprMat.jld"
-    tick()
-    importGeneExpGeneLists(normGeneExprFile,targGeneFile,potRegFile,geneExprMat,tfaGeneFile)
-    tock()
 
-    ## 2. Given a prior of TF-gene interactions, estimate transcription factor activities (TFAs) using prior-based TFA and TF mRNA levels
-    println("2. integratePrior_estTFA.jl")
-    tfaMat= instabilitiesDir * "/tfaMat.jld"
-    tick()
-    integratePrior_estTFA(geneExprMat,priorFile,minTargets,edgeSS, tfaMat)
-    tock()
-
+    # geneExprMat = tempGeneExprMat
+    # tfaMat = tempTFAMat
     ## 3. 
     println("3. estimateInstabilitiesTRNbStARS.jl")
-    netSummary = "bias" * string(100*lambdaBias) * tfaOpt
-    instabOutMat = instabilitiesDir * "/instabOutMat.jl"
+    # netSummary = "bias" * string(100*lambdaBias) * tfaOpt
+    netSummary = "bias" * join(string.(Int.(100 .* lambdaBias)), "_")
+    instabOutMat = instabilitiesDir * "/instabOutMat.jld"
+
     tick()
     estimateInstabilitiesTRNbStARS(geneExprMat,tfaMat,lambdaBias,tfaOpt,
         totSS,targetInstability,lambdaMin,lambdaMax,totLogLambdaSteps,
-        subsampleFrac,instabOutMat,leaveOutSampleList,bStarsTotSS,extensionLimit, priorFile_penalties)
+        subsampleFrac,instabOutMat,leaveOutSampleList,bStarsTotSS,extensionLimit, priorFilePenalties)
     tock()
+
     ## 4. For a given instability cutoff and model size, rank TF-geneinteractions, and calculate stabilities 
     try # not all priors have merged TFs and merged TF files
         isfile(priorMergedTfsFile) 
     catch
         global priorMergedTfsFile = ""
     end
+
     networkDir = instabilitiesDir
     instabSource = "Network"
     trnOutMat = networkDir * "/" * netSummary
     outNetFileSparse = networkDir * "/" * netSummary * "_sp.tsv"
-    networkHistDir = networkDir * "Histograms"
-    subsampHistPdf = networkHistDir * netSummary * "_ssHist"
+    networkHistDir = networkDir * "/Histograms"
+    subsampHistPdf = networkHistDir * "/" * netSummary * "_ssHist"
     outMat = networkDir * "/trnOutMat.jld"
     println("4. buildTRNs_mLassoStARS.m")
     tick()
-    buildTRNs_mLassoStARS(instabOutMat,tfaMat,priorMergedTfsFile, meanEdgesPerGene,targetInstability,instabSource, subsampHistPdf,trnOutMat,correlation_weight, outNetFileSparse, outMat, networkDir)
+    buildTRNs_mLassoStARS(instabOutMat,tfaMat,priorMergedTfsFile, meanEdgesPerGene,targetInstability,
+                instabSource,subsampHistPdf,trnOutMat,correlationWeight, 
+                outNetFileSparse, outMat, networkDir)
     tock()
 end
 
-combinedNetDir = "../outputsMichael/" * networkBaseName * "_Combined"
-nets2combine = ["../outputsMichael/" * networkBaseName * "_TFA/trnOutMat.jld"; "../outputsMichael/" * networkBaseName * "_TFmRNA/trnOutMat.jld"]
+# Combine networks 
+combinedNetDir = dirOut * "/Combined"
+nets2combine = nets2combine = [
+    dirOut * "/TFA" * "/trnOutMat.jld"; 
+    dirOut * "/TFmRNA" * "/trnOutMat.jld"]
 combineGRNs(meanEdgesPerGene, combineOpt, combinedNetDir, nets2combine)
-netCombinedSparse = combinedNetDir * "/combined_sp.tsv"
-combineGRNs2(combinedNetDir, normGeneExprFile, targGeneFile, potRegFile, tfaGeneFile, netCombinedSparse, edgeSS, minTargets; geneExprMat)
+netsCombinedSparse = combinedNetDir * "/combined_sp.tsv"
+combineGRNs2(combinedNetDir, normGeneExprFile, targGeneFile, potRegFile, tfaGeneFile, 
+                netsCombinedSparse, edgeSS, minTargets; geneExprMat)
